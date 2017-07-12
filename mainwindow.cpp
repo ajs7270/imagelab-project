e27de2878f0c9dd61b98a2a058bd4c7ad8927cca
JisuAn <ajs7270@naver.com>
2017-07-12 20:49:42 +0900

    clickable qlable add

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "clickablelabel.h"
 #include <iostream>
+#include <string>
+#include <QLabel>
 
+using std::string;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+
+
+    string filePath = "";
     ui->setupUi(this);
 
     //creates our new model and populate
@@ -23,10 +30,10 @@ MainWindow::MainWindow(QWidget *parent) :
 
     //filter the directory to show image files
     QStringList filters;
-    filters << "*.png";
+    filters << "*.png","*.jpeg","*.jpg";
 
-    dirModel->setNameFilters(filters);
-    dirModel->setNameFilterDisables(false);
+   // dirModel->setNameFilters(filters);
+   // dirModel->setNameFilterDisables(false);
 
     ui->treeView->setModel(dirModel);
 
@@ -47,11 +54,20 @@ MainWindow::~MainWindow()
 
 
 void MainWindow::on_pushButton_4_clicked()
-{
+{   
     std::cout<< mPath.toStdString()<<std::endl;
+    string filePath = mPath.toStdString();
+
+    QPixmap pix = QPixmap(filePath.c_str());
+    ui->label->setPixmap(pix);
 }
 
 void MainWindow::on_treeView_clicked(const QModelIndex &index)
 {
     mPath = dirModel->fileInfo(index).absoluteFilePath();
+}
+
+void MainWindow::on_label_clicked(QMouseEvent* event)
+{
+    std::cout << "label clicked!" << event->x() << ", " << event->y() << std::endl;
 }
